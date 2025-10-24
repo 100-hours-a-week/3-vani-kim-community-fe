24774f2b67425249bc99957898b6dcbc16dbdf60
Superson7 <vin990227@naver.com>
2025-10-24 18:35:56 +0900

    signup implementation

--- public/js/pages/signup.js
+++ public/js/pages/signup.js
@@ -1,5 +1,67 @@
-import { signup } from "../api/auth.js";
-import { getAnonymousPresignedUrl, uploadToS3 } from "../utils/image.js"
+import { getAnonymousPresignedUrl, uploadToS3 } from "/js/utils/image.js"
+import { signup, emailCheck, nicknameCheck } from "/js/api/auth.js";
+
+const emailInput = document.getElementById('email')
+const emailVerified = document.getElementById('emailVerified')
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+emailInput.addEventListener('blur', async (e) => {
+    const emailValue = emailInput.value;
+
+    if (emailValue) {
+
+        if (emailRegex.test(emailValue)) {
+            try{
+                await emailCheck(emailValue);
+                emailVerified.textContent = '사용 가능한 이메일입니다.';
+                emailVerified.style.color = 'green';
+                emailVerified.style.display = 'block';
+            } catch (e) {
+                emailVerified.textContent = '이미 존재하는 이메일입니다.';
+                emailVerified.style.color = 'red';
+                emailVerified.style.display = "block";
+            }
+        } else {
+            emailVerified.textContent = '올바른 이메일 형식이 아닙니다.';
+            emailVerified.style.color = 'red';
+            emailVerified.style.display = 'block';
+        }
+    } else {
+        emailVerified.textContent = '';
+        emailVerified.style.display = 'none';
+    }
+})
+
+const nicknameInput = document.getElementById('nickname')
+const nicknameVerified = document.getElementById('nicknameVerified')
+const nicknameRegex = /^[가-힣a-zA-Z0-9]+$/;
+
+nicknameInput.addEventListener('blur', async (e) => {
+    const nicknameValue = nicknameInput.value;
+    if (nicknameValue) {
+
+        if (nicknameRegex.test(nicknameValue)) {
+            try{
+                await nicknameCheck(nicknameValue);
+                nicknameVerified.textContent = '사용 가능한 닉네임입니다.';
+                nicknameVerified.style.color = 'green';
+                nicknameVerified.style.display = 'block';
+            } catch (e) {
+                nicknameVerified.textContent = '이미 존재하는 닉네임입니다.';
+                nicknameVerified.style.color = 'red';
+                nicknamelVerified.style.display = "block";
+            }
+        } else {
+            nicknameVerified.textContent = '올바른 닉네임형식이 아닙니다.';
+            nicknameVerified.style.color = 'red';
+            nicknameVerified.style.display = 'block';
+        }
+    } else {
+        nicknameVerified.textContent = '';
+        nicknameVerified.style.display = 'none';
+    }
+})
+//TODO 비밀번호 포멧확인
 
 //회원가입 폼의 동작 감지하기,
 document.getElementById('signup').addEventListener('submit', async (event) => {
@@ -42,7 +104,7 @@ document.getElementById('signup').addEventListener('submit', async (event) => {
 
         // 페이지 이동
         alert('회원가입 성공');
-        window.location.href = '/login.html'; //토큰 발급도 끝났으니 바로 메인이동
+        window.location.href = '/index'; //토큰 발급도 끝났으니 바로 메인이동하는게 좋지 않나?
     } catch (error) {
         console.error('회원가입 실패:', error);
         alert('회원가입에 실패했습니다.');
