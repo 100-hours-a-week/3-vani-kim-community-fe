aa198e94417de1dae05bf593800f0010310d135e
Superson7 <vin990227@naver.com>
2025-11-12 21:15:13 +0900

    라우팅 경로 변경, 404 handler 추가

--- server.js
+++ server.js
@@ -2,50 +2,56 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Routes
 
-// 페이지 내놔!
-//로그인
+// Auth Routes
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-})
-//회원가입
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'auth', 'pages', 'login', 'login.html'));
+});
+
 app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'signup.html'));
-})
-//목록 페이지
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'auth', 'pages', 'signup', 'signup.html'));
+});
+
+// Post Routes
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public','index.html'));
-})
-//유저 조회 TODO 구현 필요
-app.get('/user/:userId', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public','user.html'));
-})
-//개인정보
-app.get('/user/me', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public','user.html'));
-})
-//비밀번호 변경
-app.get('/user/me/password', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'update-password.html'));
-})
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-//게시글 상세 조회
-app.get('/post/:postId', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'post.html'));
-})
-//게시글 수정
-app.get('/post/:postId/edit', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'update-post.html'));
-})
-//게시글 생성
 app.get('/post', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'create-post.html'));
-})
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'posts', 'pages', 'create', 'create-post.html'));
+});
 
-//정적 미들웨어... 이게 먼저 나오면 뒤에가 처리가 안된다..
+app.get('/post/:postId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'posts', 'pages', 'detail', 'post.html'));
+});
+
+app.get('/post/:postId/edit', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'posts', 'pages', 'edit', 'update-post.html'));
+});
+
+// User Routes
+app.get('/user/me', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'user', 'pages', 'profile', 'user.html'));
+});
+
+app.get('/user/me/password', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'user', 'pages', 'settings', 'update-password.html'));
+});
+
+app.get('/user/:userId', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'src', 'features', 'user', 'pages', 'profile', 'user.html'));
+});
+
+// Static middleware - must come after route definitions
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 404 handler
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
